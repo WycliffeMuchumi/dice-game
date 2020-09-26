f49522da6bd1c4d4a2306ac3bcf433220425bd0c
WycliffeMuchumi <muchumimaina94@gmail.com>
2020-09-25 19:18:36 -0700

    dice-game

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 var scores, roundScore, activeParticipant, dice;
-scores = [0,0];
-roundScore = 0;
-activeParticipant = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+newGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function rollButton() {
     // 1.Display random number once button has been clicked
@@ -28,16 +19,58 @@ document.querySelector('.btn-roll').addEventListener('click', function rollButto
         document.querySelector('#current-' + activeParticipant).textContent = roundScore; 
     } else {
         // Next participant
-        // Using the ternary operator
-        activeParticipant === 0 ? activeParticipant = 1 : activeParticipant = 0;
-        roundScore = 0;
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        // Following method toggles active class from current participant to next participant in line
-        // after either of the participant rolls a 1
-        document.querySelector('.participant-0-panel').classList.toggle('active');
-        document.querySelector('.participant-1-panel').classList.toggle('active');
-        // Hiding the dice when an active participant rolls a 1
-        document.querySelector('.dice').style.display = 'none';
+        nextParticipant();
     }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Adding participant's current score to the global score once participant clicks on the hold button
+    scores[activeParticipant] += roundScore;
+    // Update the Game's User Interface
+    document.querySelector('#score-' + activeParticipant).textContent = scores[activeParticipant];
+    // Toggle the active class to the next participant once active participant clicks on hold button
+
+    // Check if the participant won the game
+    if (scores[activeParticipant] >= 20) {
+        document.querySelector('#name-' + activeParticipant).textContent = 'Winner!!!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.participant-' + activeParticipant + '-panel').classList.add('winner');
+        document.querySelector('.participant-' + activeParticipant + '-panel').classList.remove('active');
+    } else {
+        // Next participant
+        nextParticipant();
+    }
+});
+
+function nextParticipant() {
+    activeParticipant === 0 ? activeParticipant = 1 : activeParticipant = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.participant-0-panel').classList.toggle('active');
+    document.querySelector('.participant-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', newGame);
+
+// Initializing new game
+function newGame() {
+    scores = [0, 0];
+    activeParticipant = 0;
+    roundScore = 0;
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Participant 1';
+    document.getElementById('name-1').textContent = 'Participant 2';
+    // Removes active class from current winner after initializing new game
+    document.querySelector('.participant-0-panel').classList.remove('winner');
+    document.querySelector('.participant-1-panel').classList.remove('winner');
+    document.querySelector('.participant-0-panel').classList.remove('active');
+    document.querySelector('.participant-1-panel').classList.remove('active');
+    document.querySelector('.participant-0-panel').classList.add('active');
+}
